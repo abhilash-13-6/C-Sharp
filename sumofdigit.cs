425b1336e574a8ad48fa4f6c3770be511bfaee7d
Abhilash S Devadiga <abhilashsdevadiga@gmail.com>
2025-09-25 09:55:35 +0530

    Update and rename neatwdtry.cs to sumofdigit.cs

---
+++ sumofdigit.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int num=Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        while (num > 0)
+        {
+            sum += num % 10; 
+            num /= 10;       
+        }
+        Console.WriteLine("Sum of digits: " + sum);
+    }
+}
